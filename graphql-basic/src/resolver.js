21d42f2fa3d93d0958b062765c4b9f4e9aa907bc
Danh Luong <danhltn99@gmail.com>
2023-04-21 16:24:09 +0700

    query, mutation, subscription in graphql

---
+++ graphql-basic/src/resolver.js
@@ -0,0 +1,65 @@
+import path, { dirname } from "path";
+import { readFileSync } from "fs";
+import { books, libraries } from "./data";
+import { fileURLToPath } from "url";
+import { PubSub } from "graphql-subscriptions";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pubsub = new PubSub();
+
+let currentNumber = 0;
+function incrementNumber() {
+  currentNumber++;
+  pubsub.publish("NUMBER_INCREMENTED", { numberIncremented: currentNumber });
+  setTimeout(incrementNumber, 1000);
+}
+incrementNumber();
+
+//Resolver
+export const resolvers = {
+  Query: {
+    books() {
+      return books;
+    },
+    libraries() {
+      return libraries;
+    },
+    findBookByBranch(_, { branchName }, context) {
+      context.hello = "world";
+      return books.find((book) => book.branch === branchName);
+    },
+    currentNumber() {
+      return currentNumber;
+    }
+  },
+  Mutation: {
+    insertNewBook: (_, { book }) => {
+      books.push(book);
+      return book;
+    }
+  },
+  Subscription: {
+    numberIncremented: {
+      subscribe: () => pubsub.asyncIterator(["NUMBER_INCREMENTED"])
+    }
+  },
+  Library: {
+    books(parent) {
+      return books.filter((book) => book.branch === parent.branch);
+    }
+  },
+  Book: {
+    author(parent) {
+      return {
+        name: parent.author
+      };
+    },
+    test(parent, args, context) {
+      return "test";
+    }
+  }
+};
+
+export const typeDefs = readFileSync(path.join(__dirname, "demo.graphql"), {
+  encoding: "utf-8"
+});
